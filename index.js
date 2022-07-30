183fbf4222deff0c19c8755a18ffd30e0659d3a9
Bruno Filgueiras <francisco.souza@hp.com>
2022-07-30 15:23:20 -0300

    no need of js files.

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const patterns_1 = require("./src/patterns");
-// ProxyDesignPatternExample();
-(0, patterns_1.ObserverDesingPatternExample)();
